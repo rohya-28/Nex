f3b047fc8034ef4f256da420b5f31bd6c03abea5
Rohit Arsude <70759410+rohya-28@users.noreply.github.com>
2022-01-04 09:24:26 -0500

    Update fetchapi.js

--- utils/fetchapi.js
+++ utils/fetchapi.js
@@ -2,10 +2,6 @@ import axios from 'axios';
 
 export const baseUrl= 'https://bayut.p.rapidapi.com'
 
-// headers: {
-//     'x-rapidapi-host': 'bayut.p.rapidapi.com',
-//     'x-rapidapi-key': '736c575070mshcc462854d46f0a9p195438jsn9cde6e13a7de'
-//   }
 
 export const fetchApi = async (url) => {
     const {data} = await axios.get((url), {
