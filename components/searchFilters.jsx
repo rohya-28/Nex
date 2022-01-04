e2226a8645b13c1f7d6233c2c09d7316fad27cf9
Rohit Arsude <70759410+rohya-28@users.noreply.github.com>
2022-01-04 09:31:00 -0500

    Update searchFilters.jsx

--- components/searchFilters.jsx
+++ components/searchFilters.jsx
@@ -83,7 +83,7 @@ return (
                 ))}
                 {!loading && !locationData?.length && (
                   <Flex justifyContent='center' alignItems='center' flexDir='column' marginTop='5' marginBottom='5' >
-                    <Image src={noresult} />
+                    <Image src={noresult} alt="" />
                     <Text fontSize='xl' marginTop='3'>
                       Waiting to search!
                     </Text>
